755fb958f6e0660f5a74efac3f45da1d690ef862
Camilo-J <josecamilo1902@outlook.com>
2023-01-10 16:01:39 -0500

    Update insomnia file

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,22 @@ Rails.application.routes.draw do  # Define your application routes per the DSL i
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      # Rewrite controllers of devise for model User
       mount_devise_token_auth_for 'User', at: 'user', controllers: {
         sessions: 'api/v1/overrides_user/sessions',
         registrations: 'api/v1/overrides_user/registrations'
       }
 
+      # Rewrite controllers of devise for model company
       mount_devise_token_auth_for 'Company', at: 'company', controllers: {
         sessions: 'api/v1/overrides_company/sessions',
         registrations: 'api/v1/overrides_company/registrations'
       }
 
+      # POST /companies/:company_id/followings
       resources :companies, except: %i[new edit] do
         resources :followings, except: %i[new edit]
       end
-      # POST /companies/:company_id/followings
       # POST /jobs/:job_id/followings
       resources :jobs, except: %i[new edit] do
         resources :followings, except: %i[new edit]
