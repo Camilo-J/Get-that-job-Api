5138e282b6a33994a2b982208cb070fe1fa44655
Camilo-J <josecamilo1902@outlook.com>
2023-01-12 13:02:40 -0500

    Add serializer for job model

---
+++ app/serializers/job_serializer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class JobSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :category, :min_salary, :max_salary, :requirements, :optional_requirements,
+             :state, :created_at, :updated_at, :company_data
+end
+
+def company_data
+  { name: company.name,
+    email: company.email,
+    website: company.website }
+end
