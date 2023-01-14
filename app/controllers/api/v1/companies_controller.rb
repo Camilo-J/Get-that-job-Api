3ecc8ea9698a8d356ba7bc953c584c433ec4aebc
Camilo-J <josecamilo1902@outlook.com>
2023-01-14 13:18:53 -0500

    Add method to update a file(image) & routes

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -6,6 +6,21 @@ module Api
         company = Company.find(current_api_v1_company.id)
         render json: company
       end
+
+      def update
+        company = Company.find(current_api_v1_company.id)
+
+        if company.update(job_params)
+          render json: company, status: :created
+        else
+          render json: { errors: company.errors.full_messages },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def job_params
+        params.permit(:description, :name, :website, :email, :profile)
+      end
     end
   end
 end
--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :email, :description, :website, :name # , :profile
+  attributes :id, :email, :description, :website, :name, :profile_image
 end
 
 def profile_image
-  # profile&.service_url ||
-  Rails.application.routes.url.helpers.url_for(profile)
+  profile&.url
+  # Rails.application.routes.url_helpers.rails_representation_url(profile, disposition: 'attachment', only_path: true)
 end
