04d9b3063ded4722a72f3a8540516e00ac47fa78
beatriz secca <betty_hana.haru@hotmail.com>
2023-01-16 15:08:10 -0500

    Metod to close applications of a closed job

--- app/models/applications_job.rb
+++ app/models/applications_job.rb
@@ -9,5 +9,14 @@ class ApplicationsJob < ApplicationRecord
   validates :user_id, presence: true
   validates :job_id , presence: true, uniqueness: {scope: :user_id}
 
+  # after_create  :close_application_actual
   # Callbacks
+
+  # def close_application_actual
+  #   # si job tiene el estado cerrado, entonces las aplicaciones cambian su estado a finished
+  #   puts "cerrando applications"
+  #   if state != "finished" && Job.find_by(id: job_id).state == "closed"
+  #     state = "finished";
+  #   end
+  # end
 end
