54a7582c53daac040d4c3cfda119c02cc2affc6a
beatriz secca <betty_hana.haru@hotmail.com>
2023-01-16 13:04:25 -0500

    Validations for followings

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,10 @@
 class Following < ApplicationRecord
+  # Associations
   belongs_to :user, counter_cache: true
   belongs_to :followable, polymorphic: true
+
+  # Validations
+  validates :user_id, presence:true, uniqueness: {scope: [:followable_type, :followable_id]}
+
+  # Callbacks
 end
