532b90be4286a70a31011c0be7084f37c8ff5029
beatriz secca <betty_hana.haru@hotmail.com>
2023-01-16 13:03:47 -0500

    Validatios for applications_jobs

--- app/models/applications_job.rb
+++ app/models/applications_job.rb
@@ -4,4 +4,10 @@ class ApplicationsJob < ApplicationRecord
   belongs_to :job
 
   enum state: { review: 0, progress: 1, finished: 2, declined: 3 }
+  
+  # Validations
+  validates :user_id, presence: true
+  validates :job_id , presence: true, uniqueness: {scope: :user_id}
+
+  # Callbacks
 end
