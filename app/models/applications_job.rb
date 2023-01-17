3a0070e86f20d1ec8e54c3b5559bdef6ea9e522f
Camilo-J <josecamilo1902@outlook.com>
2023-01-17 16:04:04 -0500

    Add active_storage to save Cvs & contiditional

--- app/models/applications_job.rb
+++ app/models/applications_job.rb
@@ -4,14 +4,14 @@ class ApplicationsJob < ApplicationRecord
   belongs_to :job
 
   enum state: { review: 0, progress: 1, finished: 2, declined: 3 }
-  
+
   # Validations
   validates :user_id, presence: true
-  validates :job_id , presence: true, uniqueness: {scope: :user_id}
+  validates :job_id, presence: true, uniqueness: { scope: :user_id }
 
   # after_create  :close_application_actual
   # Callbacks
-
+  has_one_attached :user_cv
   # def close_application_actual
   #   # si job tiene el estado cerrado, entonces las aplicaciones cambian su estado a finished
   #   puts "cerrando applications"
