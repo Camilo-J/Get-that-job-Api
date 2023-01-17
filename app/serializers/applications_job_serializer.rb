7b90877ba43b1fad79f3746654fae94b293ce2a2
Camilo-J <josecamilo1902@outlook.com>
2023-01-16 22:04:48 -0500

    Add serializer for applications & Fix bugs

--- app/serializers/applications_job_serializer.rb
+++ app/serializers/applications_job_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationsJobSerializer < ActiveModel::Serializer
-  attributes :id, :interest, :job_id, :user_id, :state, :created_at, :updated_at, :user_data
+  attributes :id, :interest, :job_id, :user_id, :state, :created_at, :updated_at, :user_data, :job_data
 end
 
 def user_data
@@ -10,5 +10,19 @@ def user_data
     email: user.email,
     linkedin: user.linkedin,
     phone: user.phone,
-    title: user.title }
+    title: user.title,
+    curriculum: user.curriculum&.url }
+end
+
+def job_data
+  {
+    name: job.name,
+    category: job.category,
+    type: job.type_job,
+    min_salary: job.min_salary,
+    max_salary: job.max_salary,
+    created_at: job.created_at,
+    updated_at: job.updated_at,
+    company_name: job.company.name
+  }
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :phone, :name, :birthdate, :linkedin, :title, :experience, :education, :follows_company,:follows_jobs # , :user_cv
+  attributes :id, :email, :phone, :name, :birthdate, :linkedin, :title, :experience, :education, :follows_company,
+             :follows_jobs, :user_cv
 end
 
 def user_cv
-  curriculum.service_url
+  curriculum&.url
 end
 
 def follows_company
- follows = followings.map do |follow|
-next unless follow.followable_type == "Company"
+  follows = followings.map do |follow|
+    next unless follow.followable_type == 'Company'
+
     {
       id: follow.id,
       type: follow.followable_type,
       job_id: follow.followable_id,
-     # user_id: follow.user_id,
+      # user_id: follow.user_id,
       data: { name: follow.followable.name,
-                   profile: follow.followable.profile&.url,
-                   jobs: follow.followable.jobs.where(state: 0) }
+              profile: follow.followable.profile&.url,
+              jobs: follow.followable.jobs.where(state: 0) }
     }
   end
-    follows&.reject(&:nil?)
+  follows&.reject(&:nil?)
 end
 
-
 def follows_jobs
- follows = followings.map do |follow|
-next unless follow.followable_type == "Job"
+  follows = followings.map do |follow|
+    next unless follow.followable_type == 'Job'
+
     {
       id: follow.id,
       type: follow.followable_type,
       job_id: follow.followable_id,
-     # user_id: follow.user_id,
+      # user_id: follow.user_id,
       data: follow.followable
     }
   end
-    follows&.reject(&:nil?)
+  follows&.reject(&:nil?)
 end
