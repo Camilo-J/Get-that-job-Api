4a66c3b77e511e87300dc1136ac2eb345fb53dc4
Camilo-J <josecamilo1902@outlook.com>
2023-01-16 19:06:41 -0500

    Add serializer to get followsJobs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :phone, :name, :birthdate, :linkedin, :title, :experience, :education, :follows_company # , :user_cv
+  attributes :id, :email, :phone, :name, :birthdate, :linkedin, :title, :experience, :education, :follows_company,:follows_jobs # , :user_cv
 end
 
 def user_cv
@@ -19,6 +19,21 @@ next unless follow.followable_type == "Company"
       data: { name: follow.followable.name,
                    profile: follow.followable.profile&.url,
                    jobs: follow.followable.jobs.where(state: 0) }
+    }
+  end
+    follows&.reject(&:nil?)
+end
+
+
+def follows_jobs
+ follows = followings.map do |follow|
+next unless follow.followable_type == "Job"
+    {
+      id: follow.id,
+      type: follow.followable_type,
+      job_id: follow.followable_id,
+     # user_id: follow.user_id,
+      data: follow.followable
     }
   end
     follows&.reject(&:nil?)
