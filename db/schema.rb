c593558d406db238870f0e770f141e99aaec3bd9
Camilo-J <josecamilo1902@outlook.com>
2023-01-17 16:02:57 -0500

    Add column user_experience for applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_133036) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_161057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_133036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "interest"
+    t.text "user_experience"
     t.index ["job_id"], name: "index_applications_jobs_on_job_id"
     t.index ["user_id"], name: "index_applications_jobs_on_user_id"
   end
